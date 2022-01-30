061ba1cc0c857e47657ce02e6fae15dca120a5f0
MarceloJay <jaytilangus@gmail.com>
2022-01-30 10:59:04 -0300

    edit-order-fix

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import os
+import json
 import shutil
-import this
-import time
 import xml.etree.ElementTree as ET
+import requests
+import xmltodict as xmltodict
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from ui_main import Ui_Mainwindow
+from requests.exceptions import ConnectionError
 
 import sys
 
@@ -23,50 +24,56 @@ class MainWindow(QMainWindow, Ui_Mainwindow):
         self.btn_folder_select.clicked.connect(self.open_path)
         self.btn_open.clicked.connect(self.select_order)
         self.send_order.clicked.connect(self.send_order_xml)
+        self.reset_order.clicked.connect(self.edit_order)
+        self.send_order_api.clicked.connect(self.sendOrderApi)
 
     def select_order(self):
+
         self.file = QFileDialog.getOpenFileName(self, str("Select Order Xml"),
                                                 '/home/jaydev/kdsServer',
                                                 "", "(*.xml)")
-        global file_xml
-        # print(str(self.file))
-        # print(str(self.file).split(",")[0])
-        # print(str(self.file)[25:39])
+
+        global file_xml, orderId, transtype, orderStatus, kdsStation
         self.file = (str(self.file)[2:39])
         self.order_path.setText(self.file)
         self.file = str(self.file)
         file_xml = self.file
 
-        # orderXML = ET.parse(file_xml)
-        #
-        # root = orderXML.getroot()
-        # filtro = "*"
-        # for child in root.iter(filtro):
-        #     print(child.tag, child.text)
-        #     if child.tag == "KDSStation":
-        #         print(child.text)
-        #         kdsId = child.text
-        #         self.kds_station_select.setSpecialValueText(kdsId)
-        #         continue
-        #     if child.tag == "ID":
-        #         print(child.text)
-        #         orderId = child.text
-        #         self.id_select.setSpecialValueText(orderId)
-        #         continue
-        #     if child.tag == "TransType":
-        #         print(child.text)
-        #         kdsId = child.text
-        #         self.transtype_select.setSpecialValueText(kdsId)
-        #         continue
-        #     if child.tag == "OrderStatus":
-        #         print(child.text)
-        #         kdsId = child.text
-        #         self.order_status_select.setSpecialValueText(kdsId)
-        #         break
+        orderXML = ET.parse(file_xml)
+        root = orderXML.getroot()
+        filtro = "*"
+        for child in root.iter(filtro):
+
+            if child.tag == "ID":
+                print(child.text)
+                orderId = child.text
+                print(child.text)
+                self.id_select.setSpecialValueText(orderId)
+                child.text = str(orderId)
+                continue
+            if child.tag == "TransType":
+                print(child.text)
+                transtype = child.text
+                self.transtype_select.setSpecialValueText(transtype)
+                # child.text = str(orderId)
+                continue
+            if child.tag == "OrderStatus":
+                print(child.text)
+                orderStatus = child.text
+                self.order_status_select.setSpecialValueText(orderStatus)
+                break
+
+        for child in root.iter(filtro):
+            if child.tag == "KDSStation":
+                print(child.text)
+                kdsStation = child.text
+                self.kds_station_select.setSpecialValueText(kdsStation)
+                break
 
         return file_xml
 
     def open_path(self):
+
         self.file = QFileDialog.getExistingDirectory(self, str("Select Remote Folder"),
                                                      '/home/jaydev/kdsServer',
                                                      QFileDialog.ShowDirsOnly |
@@ -78,6 +85,57 @@ class MainWindow(QMainWindow, Ui_Mainwindow):
         print(path_remote_folder)
         return path_remote_folder
 
+    def edit_order(self):
+
+        global newOrderFileName, orderIdObj
+        serverFolder = path_remote_folder + "/"
+        print("serverFolder: " + serverFolder)
+        orderDefaultFileName = file_xml
+        print("orderDefaultFileName: " + orderDefaultFileName)
+
+        orderIdStart = 1
+        orderIdEnds = 1
+
+        for orderIdActual in range(orderIdStart, orderIdEnds + 1):
+            newOrderFileName = "Jay_" + str(orderIdActual) + ".xml"
+
+            print("Loading Order: " + newOrderFileName)
+
+            orderXML = ET.parse(orderDefaultFileName)
+
+            orderObj = orderXML.find('Order')
+
+            orderId = orderObj.find('ID')
+            print(orderId.text)
+            a1 = self.id_select.value()
+            print(str(a1))
+            if (a1 != 0):
+                orderId.text = str(a1)
+
+            transtype = orderObj.find('TransType')
+            a2 = self.transtype_select.value()
+            if (a2 != 0):
+                transtype.text = str(a2)
+
+            orderItem = orderObj.find('Item')
+            kdsstation = orderItem.find('KDSStation')
+            listhastation = list(map(lambda x: [kdsstation] * len(orderObj), orderObj))
+            print(str(listhastation))
+            a3 = self.kds_station_select.value()
+            if (a3 != 0):
+                for orderItem in orderObj:
+                    for kdsstation in orderItem:
+                        kdsstation.text = str(a3)
+
+            OrderStatus = orderObj.find('OrderStatus')
+            a4 = self.order_status_select.value()
+            if (a4 != 0):
+                OrderStatus.text = str(a4)
+
+            # Save the new orderset
+            orderXML.write(newOrderFileName)
+            print("Save the new order")
+
     def send_order_xml(self):
         serverFolder = path_remote_folder + "/"
         print("serverFolder: " + serverFolder)
@@ -88,25 +146,38 @@ class MainWindow(QMainWindow, Ui_Mainwindow):
         orderIdEnds = 1
 
         for orderIdActual in range(orderIdStart, orderIdEnds + 1):
-            newOrderFileName = "order_" + str(orderIdActual) + ".xml"
-
-            print("Loading Order: " + newOrderFileName)
-
-            orderXML = ET.parse(orderDefaultFileName)
-
-            orderObj = orderXML.find('Order')
-
-            orderIdObj = orderObj.find('ID')
-
-            orderIdObj.text = str(orderIdActual)
-
-            # Save the new order
-            orderXML.write(newOrderFileName)
-            print("Save the new order")
+            global remoteFolder
+            newOrderFileName = "Jay_" + str(orderIdActual) + ".xml"
 
             # Move the new order to Server folder
             shutil.move(newOrderFileName, serverFolder + newOrderFileName)
+            remoteFolder = serverFolder + newOrderFileName
             print("SMove the new order to Server folder")
+
+    def sendOrderApi(self):
+        print("SEND ORDER API")
+        try:
+            api_url = "https://dev.kdsgo.com/api/orders/new"
+
+            tok = {
+                'token': "9097f2ee3cd45a766ab43bb1db501e09a96a3281646d8a92ff89a8a578b0113b"
+                }
+
+            print(str(remoteFolder))
+            with open(remoteFolder) as xml_file:
+                data = xmltodict.parse(xml_file.read())
+                json_data = json.dumps(data)
+
+            with open("data.json", "r+") as json_file:
+                json_file.write(json_data)
+
+                print(api_url, json_file, tok)
+                r = requests.post(api_url, json_file, tok)
+                print(r)
+        except ConnectionError as e:  # This is the correct syntax
+                print("Request Error: "+e)
+
+        return requests
 
 
 if __name__ == "__main__":
